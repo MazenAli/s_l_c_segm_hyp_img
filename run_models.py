9ee53aeddad0e336826a2355b46e3643456e5959
EC2 Default User <ec2-user@ip-10-0-3-241.eu-west-1.compute.internal>
2025-01-27 17:33:59 +0000

    add compose and run script

---
+++ run_models.py
@@ -0,0 +1,15 @@
+from tensorflow.keras.models import load_model
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import time
+import patchify
+from utils_for_data_handling import *
+
+
+def main():
+    utils_o=utils_for_data_handling() # Create object of local class
+
+if __name__ == "__main__":
+    main()
